7b0053c8f1080792ed36433e46e57f7b2b093e08
Nunny R <nunnyreyes@gmail.com>
2017-12-03 01:58:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,21 +11,20 @@ function forLoop(array) {
 }
 
 
-function forLoop(array){
-  for(var i = 0; i <25; i++){
-    if (i === 1){
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
+function whileLoop(number) {
+  while (number > 0) {
+    number--
+    console.log(number)
   }
-}
-return array
+  return ("done")  
 }
 
-// function whileLoop(number) {
-//   while (number > 0) {
-//     number--
-//     console.log(number)
-//   }
-//   return ("done")
-// }
+function maybeTrue() {
+  return Math.random() >= 0.5
+  }
+  function doWhileLoop(array){
+    do {
+      array.pop()
+    } while (array.length>0 && maybeTrue())
+        return array
+    }
